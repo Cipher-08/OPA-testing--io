6dd01121f0fcb9c26ab1c1df6c1c1078c6d3420b
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-10-05 18:16:16 +0530

    Update main.tf

--- test/main.tf
+++ test/main.tf
@@ -1,5 +1,38 @@
-resource "null_resource" "foobar" {
-  provisioner "local-exec" {
-    command = "echo foobar"
+
+resource "aws_s3_bucket" "vulnerable_bucket" {
+  bucket = "unique-s3-bucket-${random_id.bucket_id.hex}"
+
+  acl    = "public-read" # Vulnerability: Bucket is publicly readable
+
+  versioning {
+    enabled = false # Vulnerability: Versioning is not enabled
   }
+
+  lifecycle_rule {
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "GLACIER"
+    }
+    expiration {
+      days = 365
+    }
+  }
+
+  logging {
+    target_bucket = "" # Vulnerability: No logging enabled
+  }
+
+  tags = {
+    Name        = "Vulnerable S3 Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 8
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.vulnerable_bucket.bucket
 }
